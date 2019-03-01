39e688ec55e4a3e69176ddd1104d838958d86578
Wojciech Gawronski <wgawronski@white-rook.pl>
2019-03-01 06:56:57 +0100

    Updated TZ custom assertion.

--- assignment/tests/spec/type/timezone.rb
+++ assignment/tests/spec/type/timezone.rb
@@ -1,31 +1,30 @@
-module Serverspec
-  module Type
+module Serverspec::Type
 
-    class TimeZone < Base
+  class TimeZone < Base
 
-      def initialize
-        @name = "Timezone"
-      end
-
-      def content
-        get_tz
-      end
-
-      def utc?
-        get_tz == "UTC"
-      end
-
-      private
-      def get_tz
-        @runner.run_command('date +"%Z %z"')
-      end
-
+    def initialize
+      super("Timezone")
     end
 
-    def timezone
-      TimeZone.new()
+    def content
+      get_tz
     end
+
+    def utc?
+      get_tz == "UTC"
+    end
+
+    private
+    def get_tz
+      @runner.run_command('date +"%Z %z"')
+    end
+
   end
+
+  def timezone
+    TimeZone.new()
+  end
+
 end
 
 include Serverspec::Type
